d9707d32d40fe27b7686e9f1acc02f6567dd3285
Gsantiago31 <84323828+Gsantiago31@users.noreply.github.com>
2021-11-20 13:40:48 -0500

    CreaciÃón controladores

--- ms-concesionario-productos/src/models/vehiculo.model.ts
+++ ms-concesionario-productos/src/models/vehiculo.model.ts
@@ -2,6 +2,8 @@ import {Entity, model, property, belongsTo, hasMany} from '@loopback/repository'
 import {Marca} from './marca.model';
 import {Categoria} from './categoria.model';
 import {CategoriaVehiculo} from './categoria-vehiculo.model';
+import {Foto} from './foto.model';
+import {Proveedor} from './proveedor.model';
 
 @model()
 export class Vehiculo extends Entity {
@@ -59,6 +61,12 @@ export class Vehiculo extends Entity {
 
   @hasMany(() => Categoria, {through: {model: () => CategoriaVehiculo, keyFrom: 'id_vehiculo', keyTo: 'id_categoria'}})
   categorias: Categoria[];
+
+  @hasMany(() => Foto, {keyTo: 'id_vehiculo'})
+  fotos: Foto[];
+
+  @belongsTo(() => Proveedor, {name: 'proveedor'})
+  id_proveedor: number;
 
   constructor(data?: Partial<Vehiculo>) {
     super(data);
--- ms-concesionario-productos/src/repositories/vehiculo.repository.ts
+++ ms-concesionario-productos/src/repositories/vehiculo.repository.ts
@@ -1,10 +1,12 @@
 import {inject, Getter} from '@loopback/core';
-import {DefaultCrudRepository, repository, BelongsToAccessor, HasManyThroughRepositoryFactory} from '@loopback/repository';
+import {DefaultCrudRepository, repository, BelongsToAccessor, HasManyThroughRepositoryFactory, HasManyRepositoryFactory} from '@loopback/repository';
 import {MysqlDataSource} from '../datasources';
-import {Vehiculo, VehiculoRelations, Marca, Categoria, CategoriaVehiculo} from '../models';
+import {Vehiculo, VehiculoRelations, Marca, Categoria, CategoriaVehiculo, Foto, Proveedor} from '../models';
 import {MarcaRepository} from './marca.repository';
 import {CategoriaVehiculoRepository} from './categoria-vehiculo.repository';
 import {CategoriaRepository} from './categoria.repository';
+import {FotoRepository} from './foto.repository';
+import {ProveedorRepository} from './proveedor.repository';
 
 export class VehiculoRepository extends DefaultCrudRepository<
   Vehiculo,
@@ -19,10 +21,18 @@ export class VehiculoRepository extends DefaultCrudRepository<
           typeof Vehiculo.prototype.id
         >;
 
+  public readonly fotos: HasManyRepositoryFactory<Foto, typeof Vehiculo.prototype.id>;
+
+  public readonly proveedor: BelongsToAccessor<Proveedor, typeof Vehiculo.prototype.id>;
+
   constructor(
-    @inject('datasources.Mysql') dataSource: MysqlDataSource, @repository.getter('MarcaRepository') protected marcaRepositoryGetter: Getter<MarcaRepository>, @repository.getter('CategoriaVehiculoRepository') protected categoriaVehiculoRepositoryGetter: Getter<CategoriaVehiculoRepository>, @repository.getter('CategoriaRepository') protected categoriaRepositoryGetter: Getter<CategoriaRepository>,
+    @inject('datasources.Mysql') dataSource: MysqlDataSource, @repository.getter('MarcaRepository') protected marcaRepositoryGetter: Getter<MarcaRepository>, @repository.getter('CategoriaVehiculoRepository') protected categoriaVehiculoRepositoryGetter: Getter<CategoriaVehiculoRepository>, @repository.getter('CategoriaRepository') protected categoriaRepositoryGetter: Getter<CategoriaRepository>, @repository.getter('FotoRepository') protected fotoRepositoryGetter: Getter<FotoRepository>, @repository.getter('ProveedorRepository') protected proveedorRepositoryGetter: Getter<ProveedorRepository>,
   ) {
     super(Vehiculo, dataSource);
+    this.proveedor = this.createBelongsToAccessorFor('proveedor', proveedorRepositoryGetter,);
+    this.registerInclusionResolver('proveedor', this.proveedor.inclusionResolver);
+    this.fotos = this.createHasManyRepositoryFactoryFor('fotos', fotoRepositoryGetter,);
+    this.registerInclusionResolver('fotos', this.fotos.inclusionResolver);
     this.categorias = this.createHasManyThroughRepositoryFactoryFor('categorias', categoriaRepositoryGetter, categoriaVehiculoRepositoryGetter,);
     this.registerInclusionResolver('categorias', this.categorias.inclusionResolver);
     this.tiene_marca = this.createBelongsToAccessorFor('tiene_marca', marcaRepositoryGetter,);
