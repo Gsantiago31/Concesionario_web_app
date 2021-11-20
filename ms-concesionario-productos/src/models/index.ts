71302601d6a2fba1a68046ceaa567d9ff08c9c81
Gsantiago31 <84323828+Gsantiago31@users.noreply.github.com>
2021-11-20 11:37:52 -0500

    Creacion modelos_1

---
+++ ms-concesionario-productos/src/models/index.ts
@@ -0,0 +1,6 @@
+export * from './marca.model';
+export * from './vehiculo.model';
+export * from './categoria-vehiculo.model';
+export * from './categoria.model';
+export * from './foto.model';
+export * from './proveedor.model';
