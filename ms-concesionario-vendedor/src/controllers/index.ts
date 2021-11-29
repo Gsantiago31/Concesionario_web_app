65c7485aaad318ada1b483d0ca6130b4d3aecbf9
Gsantiago31 <84323828+Gsantiago31@users.noreply.github.com>
2021-11-28 20:35:18 -0500

    Microservicio Vendedor (Modelos,Relaciones,Bd)

---
+++ ms-concesionario-vendedor/src/controllers/index.ts
@@ -0,0 +1,3 @@
+export * from './ping.controller';
+export * from './cliente-vendedor.controller';
+export * from './vendedor-usuario-vendedor.controller';
---
+++ ms-concesionario-vendedor/src/models/vendedor.model.ts
@@ -0,0 +1,49 @@
+import {Entity, model, property, hasOne} from '@loopback/repository';
+import {UsuarioVendedor} from './usuario-vendedor.model';
+
+@model()
+export class Vendedor extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  documento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @hasOne(() => UsuarioVendedor, {keyTo: 'id_vendedor'})
+  tiene_un: UsuarioVendedor;
+
+  constructor(data?: Partial<Vendedor>) {
+    super(data);
+  }
+}
+
+export interface VendedorRelations {
+  // describe navigational properties here
+}
+
+export type VendedorWithRelations = Vendedor & VendedorRelations;
